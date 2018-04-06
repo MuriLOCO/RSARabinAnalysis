2cb20b0fbfd7cbf242b4d7fca9102a9a955baf47
ezmenjo <jose.murilo.mendes@ericsson.com>
2018-04-06 17:43:45 -0400

    Implementation of Rabin and compatible key for RSA and Rabin; Improvement of code.

--- src/main/java/com/concordia/RSARabinAnalysis/RsaRabinAnalysisApplication.java
+++ src/main/java/com/concordia/RSARabinAnalysis/RsaRabinAnalysisApplication.java
@@ -1,5 +1,6 @@
 package com.concordia.RSARabinAnalysis;
 
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.interfaces.RSAPublicKey;
@@ -13,48 +14,68 @@ import com.concordia.RSARabinAnalysis.utils.Utils;
 @SpringBootApplication
 public class RsaRabinAnalysisApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(RsaRabinAnalysisApplication.class, args);		
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(RsaRabinAnalysisApplication.class, args);
+    // rabinEncrptDecrypt(512);
+    rsaEncryptDecrypt(512);
+    rsaEncryptDecrypt(1024);
+    rsaEncryptDecrypt(2048);
+    rsaEncryptDecrypt(4096);
+    rsaEncryptDecrypt(8192);
+    rsaEncryptDecrypt(16384);
+  }
 
-		rsaEncryptDecrypt(512);
-		rsaEncryptDecrypt(1024);
-		rsaEncryptDecrypt(2048);
-		rsaEncryptDecrypt(4096);
-		rsaEncryptDecrypt(8192);
-	}
-	
-	private static void rsaEncryptDecrypt(int keySize) throws Exception {
-		System.out.println("------" + keySize + " key size! ------");
-		
-		long startTimeKeyCreation = System.nanoTime();
-		KeyPair keyPair = Utils.buildKeyPair(keySize);    
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) keyPair.getPrivate();
-        long endTimeKeyCreation = System.nanoTime();
-        long durationKeyCreation = (endTimeKeyCreation - startTimeKeyCreation)/1000000;
-        System.out.println("Key creation time: " + durationKeyCreation);
-        
-        
-        System.out.println("Public key: " + publicKey);
-        System.out.println("Private key p: " + privateKey.getPrimeP());
-        System.out.println("Private key q: " + privateKey.getPrimeQ());
-        System.out.println("Private key e: " + privateKey.getPrivateExponent());
+  private static void rsaEncryptDecrypt(int keySize) throws Exception {
+    System.out.println("------" + keySize + " key size! ------");
+
+    //Creation of Key
+    long startTimeKeyCreation = System.nanoTime();
+    KeyPair keyPair = Utils.buildRSARabinKeyPair(keySize);
+    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+    RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) keyPair.getPrivate();
+    long endTimeKeyCreation = System.nanoTime();
+    long durationKeyCreation = (endTimeKeyCreation - startTimeKeyCreation) / 1000000;
+    System.out.println("Key creation time: " + durationKeyCreation);
+
+    final String text = IOUtils.toString(Thread.currentThread().getContextClassLoader()
+          .getResourceAsStream("text.txt"), "UTF-8");
+
+    //Encrypting RSA
+    long startTimeEncryptRSA = System.nanoTime();
+    BigInteger encryptedRSA = Utils.encryptRSA(publicKey, text);
+    long endTimeEncryptRSA = System.nanoTime();
+    long durationEncryptRSA = (endTimeEncryptRSA - startTimeEncryptRSA) / 1000000;
+    System.out.println("Encryption time RSA in ms: " + durationEncryptRSA);
+
+    //Encrypting Rabin
+    long startTimeEncryptRabin = System.nanoTime();
+    BigInteger encryptedRabin = Utils.encryptRabin(publicKey, text);
+    long endTimeEncryptRabin = System.nanoTime();
+    long durationEncryptRabin = (endTimeEncryptRabin - startTimeEncryptRabin) / 1000000;
+    System.out.println("Encryption time Rabin in ms: " + durationEncryptRabin);
+
     
-        final String text = IOUtils.toString(Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("text.txt"), "UTF-8");        
-        
-        long startTimeEncrypt = System.nanoTime();
-        byte [] encrypted = Utils.encrypt(privateKey, text);
-        long endTimeEncrypt = System.nanoTime();
-        long durationEncrypt = (endTimeEncrypt - startTimeEncrypt)/1000000;
-        System.out.println("Encryption time in ms: " + durationEncrypt);                
-        
-        
-        long startTimeDecrypt = System.nanoTime();
-        byte[] secret = Utils.decrypt(publicKey, encrypted);
-        long endTimeDecrypt = System.nanoTime();
-        long durationDecrypt= (endTimeDecrypt - startTimeDecrypt)/1000000;
-        System.out.println("Deryption time in ms: " + durationDecrypt);  
-        
-	}
+    //Decrypting RSA
+    long startTimeDecryptRSA = System.nanoTime();
+    Utils.decryptRSA(privateKey, encryptedRSA);
+    long endTimeDecrypt = System.nanoTime();
+    long durationDecrypt = (endTimeDecrypt - startTimeDecryptRSA) / 1000000;
+    System.out.println("Deryption time RSA in ms: " + durationDecrypt);
+
+  }
+
+  private static void rabinEncrptDecrypt(int keySize) throws Exception {
+    long startTimeKeyCreation = System.nanoTime();
+    KeyPair keyPair = Utils.buildRabinKeyPair(keySize);
+
+    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+    RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) keyPair.getPrivate();
+    long endTimeKeyCreation = System.nanoTime();
+    long durationKeyCreation = (endTimeKeyCreation - startTimeKeyCreation) / 1000000;
+    System.out.println("Key creation time: " + durationKeyCreation);
+
+    final String text = IOUtils.toString(Thread.currentThread().getContextClassLoader()
+          .getResourceAsStream("text.txt"), "UTF-8");
+
+  }
 }
--- src/main/java/com/concordia/RSARabinAnalysis/utils/Utils.java
+++ src/main/java/com/concordia/RSARabinAnalysis/utils/Utils.java
@@ -1,31 +1,43 @@
 package com.concordia.RSARabinAnalysis.utils;
 
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-
-import javax.crypto.Cipher;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPublicKey;
 
 public class Utils {
-	
-	public static KeyPair buildKeyPair(int keySize) throws NoSuchAlgorithmException {        
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(keySize);      
-        return keyPairGenerator.genKeyPair();
-    }
-	
-	public static byte[] encrypt(PrivateKey privateKey, String message) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");  
-        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-        return cipher.doFinal(message.getBytes());  
-    }
-	
-	public static byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");  
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);        
-        return cipher.doFinal(encrypted);
-    }
-	
+
+  private static final BigInteger TWO = new BigInteger("2");
+  private static final BigInteger THREE = new BigInteger("3");
+  private static final BigInteger FOUR = new BigInteger("4");
+
+  public static KeyPair buildRSARabinKeyPair(int keySize) throws NoSuchAlgorithmException {
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    keyPairGenerator.initialize(keySize);
+    KeyPair keyPair = keyPairGenerator.genKeyPair();
+
+    RSAPrivateCrtKey rsaKeyPair = (RSAPrivateCrtKey) keyPair.getPrivate();
+    if (!rsaKeyPair.getPrimeQ().mod(FOUR).equals(THREE) || !rsaKeyPair.getPrimeP().mod(FOUR).equals(THREE))
+      buildRSARabinKeyPair(keySize);
+
+    return keyPairGenerator.genKeyPair();
+  }
+
+  public static BigInteger encryptRSA(RSAPublicKey publicKey, String message) throws Exception {
+    return new BigInteger(message.getBytes()).modPow(publicKey.getPublicExponent(), publicKey.getModulus());
+  }
+
+  public static BigInteger decryptRSA(RSAPrivateCrtKey privateKey, BigInteger encrypted) throws Exception {
+    return encrypted.modPow(privateKey.getPrivateExponent(), privateKey.getModulus());
+  }
+
+  public static BigInteger encryptRabin(RSAPublicKey publicKey, String message) throws Exception {
+    BigInteger bigIntegerMessage = new BigInteger(message.getBytes());
+    BigInteger encryptedMessage = bigIntegerMessage.modPow(TWO, publicKey.getModulus());
+    return encryptedMessage;
+  }
+
+
 }
